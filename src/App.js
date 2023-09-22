07d4bf1bc15417bb4b48b0514f58c8c3c79c031d
tienpv <tienpv.dev@gmail.com>
2023-09-23 00:31:04 +0700

    EP63: Setup Bootstrap 5 vs React Router Dom v6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import logo from "./logo.svg";
-import "./App.scss";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-import MyComponent from "./components/MyComponent";
-import React from "react";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className='app-container'>
-                <MyComponent></MyComponent>
-            </div>
-        );
-    }
+import "./App.scss";
+
+
+const App = () => {
+    return (
+        <div className='app-container'>
+            Hello world
+        </div>
+    );
+
 }
-//  sadsassddssaddas
 export default App;
