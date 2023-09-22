b57e943f6d2ba7af4d2788e8a537bfe90f93b2cf
tienpv <tienpv.dev@gmail.com>
2023-09-23 01:06:27 +0700

    EP64: Deasign Header with BootStrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import "./App.scss";
-
+import Header from "./components/Header/Header";
 
 const App = () => {
     return (
         <div className='app-container'>
-            Hello world
+            <Header />
         </div>
     );
 
